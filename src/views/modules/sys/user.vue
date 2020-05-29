2020dde1a2b9883877c94025e74a4d09d5e788bb
jfsz <843400447@qq.com>
2020-05-29 23:45:03 +0800

    20200529 前端代码提交

--- src/views/modules/sys/user.vue
+++ src/views/modules/sys/user.vue
@@ -2,7 +2,7 @@
   <div class="mod-user">
     <el-form :inline="true" :model="dataForm" @keyup.enter.native="getUserList()">
       <el-form-item>
-        <el-input v-model="dataForm.username" placeholder="用户名/手机号" clearable></el-input>
+        <el-input v-model="dataForm.userName" placeholder="用户名/手机号" clearable></el-input>
       </el-form-item>
       <el-form-item>
         <el-button @click="getUserList()" type="primary" icon="el-icon-search">查询</el-button>
@@ -97,32 +97,33 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="totalPage">
     </el-pagination>
-    <saveOrUpdateUser ref="addOrUpdate" @refreshDataList="getUserList"></saveOrUpdateUser>
+    <save-or-update v-if="addOrUpdateVisible" ref="saveOrUpdate" @refreshDataList="getUserList"></save-or-update>
   </div>
 </template>
 <script>
-import saveOrUpdateUser from './user_add_or_update'
+import saveOrUpdate from './user_add_or_update'
 export default{
   data () {
     return {
       dataForm: {
-        username: ''
+        userName: ''
       },
       userList: [], // 用户列表
       selectedList: [], // 选中的数据
       userListLoading: false,
       totalPage: 0, // 记录总条数
       pageSize: 10, // 每页记录数据
-      pageIndex: 1 // 当前页面数
+      pageIndex: 1, // 当前页面数
+      addOrUpdateVisible: false // 是否显示组件saveOrUpdate
     }
+  },
+  components: {
+    saveOrUpdate
   },
   // 在vue对象存活的情况下，进入当前存在activated()函数的页面时，
   // 一进入页面就触发；可用于初始化页面数据等。这个用于每次只要进入这个组件页面就初始化页面，可以用于列表数据等的刷新
   activated () {
     this.getUserList()
-  },
-  comments: {
-    saveOrUpdateUser
   },
   methods: {
     // 获取用户列表
@@ -131,7 +132,7 @@ export default{
       let dataList = {
         page: this.pageIndex,
         limit: this.pageSize,
-        username: this.dataForm.username ? this.dataForm.username : ''
+        userName: this.dataForm.userName ? this.dataForm.userName : ''
       }
       this.$api.get('/sys/user/list', dataList)
         .then(res => {
@@ -145,10 +146,11 @@ export default{
           this.userListLoading = false
         }).catch()
     },
-    // 新增用户
+    // 新增/修改用户
     addOrUpdateUser (userId) {
+      this.addOrUpdateVisible = true
       this.$nextTick(() => {
-        this.$refs.addOrUpdate.init(userId)
+        this.$refs.saveOrUpdate.init(userId)
       })
     },
     // 选中事件
@@ -160,7 +162,6 @@ export default{
       let data = userIds ? [userIds] : this.selectedList.map(item => {
         return item.userId
       })
-      console.log(userIds)
       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
--- src/views/modules/sys/user_add_or_update.vue
+++ src/views/modules/sys/user_add_or_update.vue
@@ -1,5 +1,8 @@
 <template>
-  <el-dialog>
+  <el-dialog
+    :title="!userForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
     <el-form :model="userForm" :rules="rules" ref="userForm" @keyup.enter.native="saveOrUpdate()" label-width="80px">
       <el-form-item label="用户名" >
         <el-input v-model="userForm.userName"></el-input>
@@ -20,9 +23,23 @@
         <el-input v-model="userForm.status"></el-input>
       </el-form-item>
       <el-form-item label="角色" >
-        <el-input v-model="userForm.roleList"></el-input>
+        <el-select v-model="userForm.roleIdList"
+           multiple
+           collapse-tags
+           style="margin-left: 20px;"
+           placeholder="请选择" >
+          <el-option v-for="role in roleList"
+          :key="role.roleId"
+          :label="role.roleName"
+          :value="role.roleId">
+          </el-option>
+        </el-select>
       </el-form-item>
     </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="saveOrUpdate()">确定</el-button>
+    </span>
   </el-dialog>
 </template>
 <script>
@@ -58,6 +75,8 @@ export default{
       }
     }
     return {
+      visible: false,
+      roleList: [],
       userForm: {
         id: 0,
         userName: '',
@@ -66,7 +85,7 @@ export default{
         email: '',
         mobile: '',
         status: 1,
-        roleList: []
+        roleIdList: []
       },
       rules: {
         userName: [
@@ -90,7 +109,35 @@ export default{
   },
   methods: {
     init: function (userId) {
-
+      this.userForm.id = userId || 0
+      let data = {
+        'userId': userId
+      }
+      this.$api.get('/sys/role/queryByUserId', data)
+        .then(res => {
+          if (Object.is(res.code, 0)) {
+            this.roleList = res.list
+          }
+        }).then(res => {
+          this.visible = true
+          this.$nextTick(() => {
+            this.$refs['userForm'].resetFields()
+          })
+        }).then(res => {
+          if (this.userForm.id) {
+            this.$api.get('/sys/user/info?userId=' + this.userForm.id, null)
+              .then(res => {
+                if (Object.is(res.code, 0)) {
+                  this.userForm.userName = res.user.username
+                  this.userForm.salt = res.user.salt
+                  this.userForm.email = res.user.email
+                  this.userForm.mobile = res.user.mobile
+                  this.userForm.roleIdList = res.user.roleIdList
+                  this.userForm.status = res.user.status
+                }
+              })
+          }
+        })
     },
     saveOrUpdate: function () {
       this.$refs['userForm'].validate((valid) => {
@@ -102,7 +149,7 @@ export default{
             'email': this.userForm.email,
             'status': this.userForm.status,
             'mobile': this.userForm.mobile,
-            'userRole': this.userForm.roleList
+            'roleIdList': this.userForm.roleIdList
           }
           this.$api.post(this.userForm.id ? '/sys/user/update' : '/sys/user/save', data)
             .then(res => {
